767e52cd49122c2243105d28e2f670ed2aefe1fd
nihalmore05 <nihalmore05@gmail.com>
2025-10-24 19:35:39 +0530

    account

---
+++ OOP_P1_DAY_2_ENCAPSULATION/account.java
@@ -0,0 +1,51 @@
+package OOP_P1_DAY_2_ENCAPSULATION;
+
+//7. Create an `Account` class with private balance and secure deposit/withdraw methods.
+
+public class account {
+    
+    public String name;
+    public long accountnum;
+    private double balance;
+
+     
+
+    // Deposite Method
+    public void deposit(double amount){
+        if(amount > 0){
+            balance += amount;
+            System.out.println("Deposite :"+ amount);
+        }else{
+            System.out.println("Deposite amount must be positve");
+        }
+    }
+      // withdraw Method
+    public void withdraw(double amount){
+        if(amount <= balance && amount > 0){                // put condition for negative value and more than balance amount controlling
+            balance -= amount;
+            System.out.println("Withdraw :"+ amount);
+        }else{
+            System.out.println("Invalid amount OR Insufficient balance");
+        }
+    }
+
+    // using getter for getting return balance
+    public double getBalance() {
+        return balance;
+    }
+    
+      public static void main(String[] args) {
+        account ac = new account();
+
+        ac.deposit(5000);
+        ac.withdraw(1000);
+        ac.deposit(50000);
+        ac.withdraw(-5000);
+
+        System.out.println("Current Balance :"+ ac.balance);
+      }
+
+    
+
+
+}
