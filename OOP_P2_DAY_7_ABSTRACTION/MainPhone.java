d3fb8cad453b4f35c62e92a3bd30055af7316197
nihalmore05 <nihalmore05@gmail.com>
2025-11-20 17:15:58 +0530

    some Update

--- OOP_P2_DAY_7_ABSTRACTION/MainPhone.java
+++ OOP_P2_DAY_7_ABSTRACTION/MainPhone.java
@@ -1,4 +1,5 @@
 //8. Create abstract `Phone` with abstract `call()` and normal `message()` method.
+//9. Try to create an object of abstract class (and observe error).
 
 abstract class Phone{
     abstract void call();
