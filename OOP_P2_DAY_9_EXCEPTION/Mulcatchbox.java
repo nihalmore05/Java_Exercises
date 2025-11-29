176269f51273233234f2d14fd7cfecf31025a2ca
nihalmore05 <nihalmore05@gmail.com>
2025-11-29 17:39:08 +0530

    Finally used

--- OOP_P2_DAY_9_EXCEPTION/Mulcatchbox.java
+++ OOP_P2_DAY_9_EXCEPTION/Mulcatchbox.java
@@ -1,30 +1,30 @@
 package OOP_P2_DAY_9_EXCEPTION;
 
 //3. Use multiple catch blocks for different exceptions.
+//4. Use `finally` to close resources.
 
 public class Mulcatchbox {
-    public static void main(String[] args){
-   
-    try{
-    int a = 100, b = 0;
-    int c = a/b;                   // Arithmetic Exception occuring here becous divide by zero   
-    System.out.println(c);                    
-                                   // whe exception occuring the try block skip other code
-    String s = null;
-    System.out.println(s.length());
-}
-    catch(ArithmeticException e)
-    {
-        System.out.println("Arithmatic Error here : " + e);
-    }
-    catch(NullPointerException e)
-    {
-        System.out.println(e);
-    }
+    public static void main(String[] args) {
 
-    catch (Exception e) {
-    System.out.println("General error: " + e);
-}
+        try {
+            int a = 100, b = 0;
+            int c = a / b; // Arithmetic Exception occuring here becous divide by zero
+            System.out.println(c);
+            // whe exception occuring the try block skip other code
+            String s = null;
+            System.out.println(s.length());
+        } catch (ArithmeticException e) {
+            System.out.println("Arithmatic Error here : " + e);
+        } catch (NullPointerException e) {
+            System.out.println(e);
+        }
 
-}
+        catch (Exception e) {
+            System.out.println("General error: " + e);
+        }
+        finally{                             // used finally to close Resources.
+            System.out.println("The finally block running.....");
+        }
+
+    }
 }
